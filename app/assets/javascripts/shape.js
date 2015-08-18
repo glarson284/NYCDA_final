9b5d1de427b7d3fd506f2303410180ff7392d379
Gwen Larson <glarson284@gmail.com>
2015-08-18 16:29:58 -0400

    added drag and drop color change interact

--- app/assets/javascripts/shape.js
+++ app/assets/javascripts/shape.js
@@ -41,4 +41,41 @@ interact('.draggable')
   // this is used later in the resizing demo
   window.dragMoveListener = dragMoveListener;
 
-  
+
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '#yes-drop',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+  },
+  ondrop: function (event) {
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
+
+
+
+
